b08374120657f6c9a7e2f753d0bf77f84b56460e
jhuang2801 <jl.grace.huang@gmail.com>
2022-11-20 17:35:22 -0500

    Updating queries.sql

--- queries.sql
+++ queries.sql
@@ -46,7 +46,6 @@ CREATE TABLE titles(
 	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  	PRIMARY KEY (emp_no)
 );
 SELECT * FROM departments;
 SELECT * FROM dept_manager;
@@ -55,14 +54,20 @@ SELECT * FROM dept_emp;
 SELECT * FROM salaries;
 SELECT * FROM titles;
 
--- Retirement eligibility 
 SELECT first_name, last_name
-INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+-- Retirement eligibility
+SELECT first_name, last_name
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info;
 
 -- Number of employees retiring
 SELECT COUNT(first_name)
@@ -71,18 +76,10 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT first_name, last_name
+INTO retirement_info
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+SELECT * FROM retirement_info;
 
